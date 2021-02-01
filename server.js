11d9e150570afeb1c8d5189f7f00373261fe08c5
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2021-02-01 00:00:41 -0600

    Update server.js

--- server.js
+++ server.js
@@ -26,10 +26,6 @@ app.use(express.json());
 //! HTML Routes
 //?===================================
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
-
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
