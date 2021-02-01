f4f12195c10bea47098dc719122f912bf1ccf69f
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2021-01-31 19:09:18 -0600

    update POST and DELETE request

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+//? Dependencies
+//*=====================================
+
 const { urlencoded, json } = require("express");
 const express = require("express");
 const path = require("path");
@@ -5,19 +8,23 @@ const fs = require("fs");
 const notes = require("./db/db.json");
 const { parse } = require("path");
 
-//Express setup
-//================================
+//? Express setup
+//*================================
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//Middleware
-//===================================
+//? Middleware
+//*===================================
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//HTML Routes
-//===================================
+/* ---- ROUTES ---- */
+
+//! HTML Routes
+//?===================================
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
@@ -27,34 +34,36 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
-//API Routes
-//==================================
+//! API Routes
+//?==================================
 
 app.get("/api/notes", (req, res) => {
   // console.log(notes);
   res.json(notes);
 });
 
+//* POST Request
+//?==========================================
+
 app.post("/api/notes", (req, res) => {
-  // const makeId = notes.forEach((note, i) => {
-  //   note.id + 1;
-  // });
   const newNote = {
-    //Add id using the length of the notes array
-    //and adding 1 so it'll start at 1
+    //*Add id using the length of the notes array
+    //*and adding 1 so it'll start at 1
+
     id: notes.length + 1,
     title: req.body.title,
     text: req.body.text,
   };
 
   //console.log(newNote);
-
   notes.push(newNote);
-  console.log(notes);
+  //console.log(notes);
 
   res.json(notes);
 });
 
+//* DELETE Request
+//?==========================================
 app.delete("/api/notes/:id", (req, res) => {
   const toDelete = req.params.id;
 
@@ -66,8 +75,8 @@ app.delete("/api/notes/:id", (req, res) => {
   }
 });
 
-//Listener
-//==================================
+//? Listener
+//*==================================
 app.listen(PORT, () =>
   console.log("Server listening on http://localhost:" + PORT)
 );
